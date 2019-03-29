437d693650b03d76ed7c65d93c8940ea33baeac8
cole-gillespie <cole.gillespie@tanium.com>
2019-03-29 03:25:39 -0400

    build cache

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
 console.log('sw.js')
+
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
